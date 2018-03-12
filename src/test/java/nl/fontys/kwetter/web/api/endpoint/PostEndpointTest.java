28611602f716e49782d48ae96669819b717f6507
Tobi van Bronswijk <t.vanbronswijk@student.fontys.nl>
2018-03-12 21:42:05 +0100

    added TDD tests

--- src/test/java/nl/fontys/kwetter/web/api/endpoint/PostEndpointTest.java
+++ src/test/java/nl/fontys/kwetter/web/api/endpoint/PostEndpointTest.java
@@ -1,24 +1,53 @@
 package nl.fontys.kwetter.web.api.endpoint;
 
+import nl.fontys.kwetter.model.post.Post;
 import org.junit.Test;
 
-import static com.jayway.restassured.RestAssured.when;
-import static org.hamcrest.Matchers.equalTo;
+import java.util.Date;
+
+import static com.jayway.restassured.RestAssured.given;
 
 public class PostEndpointTest {
 
     @Test
-    public void getAllPosts() {
-        when().get("/kwetter/api/post/all").
-                then().
-                statusCode(200);
-    }
+    public void restPostTest() {
+        //create a User
+        Post newPost = new Post(null, "test", new Date(), null);
+        given().body(newPost)
+                .when()
+                .put("kwetter/api/post")
+                .then()
+                .statusCode(200);
 
-    @Test
-    public void getSinglePost() {
-        when().get("kwetter/api/post/{id}", 1).
-                then().
-                statusCode(200).
-                body("content", equalTo("This is the first post on Kwetter! #first"));
+        //get user
+        given().when()
+                .get("kwetter/api/post/1")
+                .then()
+                .statusCode(200);
+
+        //update existing user
+        Post updatePost = new Post(null, "test2", new Date(), null);
+        updatePost.setId(1L);
+        given().body(updatePost)
+                .when()
+                .put("kwetter/api/post")
+                .then()
+                .statusCode(200);
+
+        //remove user
+        Post removePost = new Post();
+        removePost.setId(1L);
+        given().body(removePost)
+                .when()
+                .delete("kwetter/api/post")
+                .then()
+                .statusCode(200);
+
+
+        //get all users
+        given().when()
+                .get("kwetter/api/post/all")
+                .then()
+                .statusCode(200);
     }
 }
--- src/test/java/nl/fontys/kwetter/web/api/endpoint/UserEndpointTest.java
+++ src/test/java/nl/fontys/kwetter/web/api/endpoint/UserEndpointTest.java
@@ -1,24 +1,51 @@
 package nl.fontys.kwetter.web.api.endpoint;
 
+import nl.fontys.kwetter.model.user.User;
 import org.junit.Test;
 
-import static com.jayway.restassured.RestAssured.when;
-import static org.hamcrest.Matchers.equalTo;
+import static com.jayway.restassured.RestAssured.given;
 
 public class UserEndpointTest {
 
     @Test
-    public void getAllUsers() {
-        when().get("/kwetter/api/user/all").
-                then().
-                statusCode(200);
-    }
+    public void restUserTest() {
+        //create a User
+        User newUser = new User("test", "test", "test@test.nl", null, null);
+        given().body(newUser)
+                .when()
+                .put("kwetter/api/user")
+                .then()
+                .statusCode(200);
 
-    @Test
-    public void getSingleUser() {
-        when().get("kwetter/api/user/{id}", 1).
-                then().
-                statusCode(200).
-                body("username", equalTo("first"));
+        //get user
+        given().when()
+                .get("kwetter/api/user/1")
+                .then()
+                .statusCode(200);
+
+        //update existing user
+        User updateUser = new User("test2", "test", "test@test.nl", null, null);
+        updateUser.setId(1L);
+        given().body(updateUser)
+                .when()
+                .put("kwetter/api/user")
+                .then()
+                .statusCode(200);
+
+        //remove user
+        User removeUser = new User();
+        removeUser.setId(1L);
+        given().body(removeUser)
+                .when()
+                .delete("kwetter/api/user")
+                .then()
+                .statusCode(200);
+
+
+        //get all users
+        given().when()
+                .get("kwetter/api/user/all")
+                .then()
+                .statusCode(200);
     }
 }
