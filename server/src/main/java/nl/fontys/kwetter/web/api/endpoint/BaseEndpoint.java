1a8f07ff2dee7db3ca2e3d5c1c393c32d0c69c43
Tobi van Bronswijk <t.vanbronswijk@student.fontys.nl>
2018-04-19 09:53:37 +0200

    Added more status codes

--- server/src/main/java/nl/fontys/kwetter/web/api/endpoint/BaseEndpoint.java
+++ server/src/main/java/nl/fontys/kwetter/web/api/endpoint/BaseEndpoint.java
@@ -3,13 +3,120 @@ package nl.fontys.kwetter.web.api.endpoint;
 import javax.ejb.Stateless;
 import javax.ws.rs.core.Response;
 
-import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+import java.net.URI;
+
+import static javax.ws.rs.core.Response.Status.*;
 
 @Stateless
 public abstract class BaseEndpoint {
 
     /**
-     * A response with an OK HTTP response
+     * Creates a {@link javax.ws.rs.core.Response} object with status 202 - ACCEPTED
+     * @return 200 - OK
+     */
+    protected Response accepted() {
+        return Response.accepted()
+                .build();
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 202 - ACCEPTED and a body
+     * @return 200 - OK
+     */
+    protected <T> Response accepted(T entity) {
+        return Response.accepted()
+                .entity(entity)
+                .build();
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 400 - BAD REQUEST
+     * @return 200 - OK
+     */
+    protected Response badRequest() {
+        return status(BAD_REQUEST);
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 400 - BAD REQUEST and a body
+     * @return 200 - OK
+     */
+    protected <T> Response badRequest(T entity) {
+        return status(BAD_REQUEST, entity);
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 201 - CREATED
+     * @return 200 - OK
+     */
+    protected Response created(URI location) {
+        return Response.created(location)
+                .build();
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 201 - CREATED and a body
+     * @return 200 - OK
+     */
+    protected <T> Response created(URI location, T entity) {
+        return Response.created(location)
+                .entity(entity)
+                .build();
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 403 - FORBIDDEN
+     * @return 401 - UNAUTHORIZED
+     */
+    protected Response forbidden(){
+        return status(FORBIDDEN);
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 403 - FORBIDDEN and a body
+     * @return 401 - UNAUTHORIZED
+     */
+    protected <T> Response forbidden(T entity){
+        return status(FORBIDDEN, entity);
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 204 - NO CONTENT
+     * @return 200 - OK
+     */
+    protected Response noContent() {
+        return Response.noContent()
+                .build();
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 204 - NO CONTENT and a body
+     * @return 200 - OK
+     */
+    protected <T> Response noContent(T entity) {
+        return Response.noContent()
+                .entity(entity)
+                .build();
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 404 - NOT FOUND
+     * @return 200 - OK
+     */
+    protected Response notFound() {
+        return status(NOT_FOUND);
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 404 - NOT FOUND and a body
+     * @return 200 - OK
+     */
+    protected <T> Response notFound(T entity) {
+        return status(NOT_FOUND, entity);
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 200 - OK
      * @return 200 - OK
      */
     protected Response ok() {
@@ -18,22 +125,39 @@ public abstract class BaseEndpoint {
     }
 
     /**
-     * A response with an OK HTTP response and an object in it's body
-     * @param entity The body of the Response
+     * Creates a {@link javax.ws.rs.core.Response} object with status 200 - OK and a body
      * @return 200 - OK
      */
-    protected Response ok(Object entity) {
+    protected <T> Response ok(T entity) {
         return Response.ok()
                 .entity(entity)
                 .build();
     }
 
     /**
-     * A response with an UNAUTHORIZED HTTP response
+     * Creates a {@link javax.ws.rs.core.Response} object with status 401 - UNAUTHORIZED
      * @return 401 - UNAUTHORIZED
      */
     protected Response unauthorized(){
-        return Response.status(UNAUTHORIZED)
+        return status(UNAUTHORIZED);
+    }
+
+    /**
+     * Creates a {@link javax.ws.rs.core.Response} object with status 401 - UNAUTHORIZED and a body
+     * @return 401 - UNAUTHORIZED
+     */
+    protected <T> Response unauthorized(T entity){
+        return status(UNAUTHORIZED, entity);
+    }
+
+    protected Response status(Response.Status statusCode){
+        return Response.status(statusCode)
+                .build();
+    }
+
+    protected <T> Response status(Response.Status statusCode, T entity){
+        return Response.status(statusCode)
+                .entity(entity)
                 .build();
     }
 }
