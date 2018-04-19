2bb9b3cf1ccc078858d00c7d1ea94e0ea54f1fc7
Tobi van Bronswijk <t.vanbronswijk@student.fontys.nl>
2018-04-19 11:47:47 +0200

    Login error handling

--- client/src/modules/views/NavBar/NavBarLogin.jsx
+++ client/src/modules/views/NavBar/NavBarLogin.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {Button, Input, Menu} from "semantic-ui-react";
+import {Button, Form, Menu} from "semantic-ui-react";
 import AuthTokenService from "../../../services/auth/AuthTokenService";
+import './NavBar.css';
 
 class NavBarLogin extends Component {
 
@@ -9,36 +10,47 @@ class NavBarLogin extends Component {
         this.state = {
             username: null,
             password: null,
+            err: null,
         };
     }
 
     handleChange(e) {
         this.setState(
             {
+                err: null,
                 [e.target.name]: e.target.value
             }
         )
     }
 
     async handleItemClick() {
-        const { username, password } = this.state;
-        await AuthTokenService.login(username, password);
-        window.location.reload();
+        const {username, password} = this.state;
+        try {
+            await AuthTokenService.login(username, password);
+            window.location.reload();
+        } catch (e) {
+            this.setState({err: e});
+        }
     }
 
     render() {
+        const {err} = this.state;
+
         return <Menu.Menu position='right'>
             <Menu.Item as='a' href='/register'>
                 <h5>Not on Kwetter? Sign up!</h5>
             </Menu.Item>
-            <Menu.Item>
-                <Input icon='user' onChange={this.handleChange.bind(this)} placeholder='Username' name='username'/>
+            <Menu.Item className='navbar-login-item'>
+                <Form size='tiny'>
+                    <Form.Group className='navbar-login-form'>
+                        <Form.Input className='navbar-login-field' icon='user' onChange={this.handleChange.bind(this)}
+                                    placeholder='Username' name='username' type='text' required error={err!=null}/>
+                        <Form.Input className='navbar-login-field' icon='lock' onChange={this.handleChange.bind(this)}
+                                    placeholder='Password' name='password' type='password' required error={err!=null}/>
+                        <Menu.Item name='Login' as={Button} onClick={this.handleItemClick.bind(this)}/>
+                    </Form.Group>
+                </Form>
             </Menu.Item>
-            <Menu.Item>
-                <Input icon='lock' onChange={this.handleChange.bind(this)} placeholder='Password'
-                       name='password' type='password'/>
-            </Menu.Item>
-            <Menu.Item name='Login' as={Button} onClick={this.handleItemClick.bind(this)}/>
         </Menu.Menu>;
     }
 }
